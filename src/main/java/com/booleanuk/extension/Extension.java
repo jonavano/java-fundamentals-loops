119a42f7978c0a2ad8a60086e571007e0bf71b8f
Jonathan van Oudheusden <jonathanvanoudheusden@gmail.com>
2025-08-05 16:14:37 +0200

    implement exercises

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -5,6 +5,14 @@ public class Extension {
 
     public void stepOne() {
         // TODO: 1. Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
+        numsEven = new int[4];
+        int index = 0;
+        for (int i = 0; i <= 6; i++) {
+            if (i%2 == 0) {
+                numsEven[index++] = i;
+
+            }
+        }
 
 
         
